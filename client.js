05f35e932094930a88ec65816c7e92055f04cab2
Benji Kay <okaybenji@gmail.com>
2017-01-24 20:57:50 -0600

    create addBox function

--- client.js
+++ client.js
@@ -1,6 +1,3 @@
-const input = document.querySelector('input');
-const box = document.querySelector('a-box');
-
 const randomIntBetween = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
@@ -17,41 +14,68 @@ const randomColor = () => {
   return color;
 };
 
-const isUrl = url =>  url.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
-
-let cachedImages = {};
-
-const testImage = (url) => {
-  if (cachedImages[url]) {
-    return new Promise(resolve => resolve(cachedImages[url]));
-  }
-
-  const timeout = 5000;
-  const img = new Image();
-  let timer;
-
-  return new Promise((resolve, reject) => {
-    img.onerror = img.onabort = () => {
-      clearTimeout(timer);
-      reject(new Error('URL is not a valid image.'));
-    };
-    img.onload = () => {
-      clearTimeout(timer);
-      cachedImages[url] = img;
-      resolve();
-    };
-    timer = setTimeout(() => {
-      img.src = '';
-      reject(new Error('Timed out while validating image.'));
-    }, timeout);
-    img.src = url;
-  });
+let box = {
+  id: 0,
+  position: '0 0.5 0',
+  rotation: '0 45 0',
+  color: randomColor()
 };
 
+const addBox = (box) => {
+  const scene = document.querySelector('a-scene');
+  const el = document.createElement('a-box');
+
+  el.setAttribute('width', '1');
+  el.setAttribute('height', '1');
+  el.setAttribute('depth', '1');
+  el.setAttribute('id', box.id);
+  el.setAttribute('position', box.position);
+  el.setAttribute('rotation', box.rotation);
+  el.setAttribute('color', box.color);
+
+  scene.appendChild(el);
+};
+
+addBox(box);
+
+const isUrl = url =>  url.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+
+let cachedUrls = {};
+
+const input = document.querySelector('input');
+
 input.addEventListener('input', () => {
+  const validateImage = (url) => {
+    if (cachedUrls[url]) {
+      return new Promise(resolve => resolve());
+    }
+
+    const timeout = 5000;
+    const img = new Image();
+    let timer;
+
+    return new Promise((resolve, reject) => {
+      img.onerror = img.onabort = () => {
+        clearTimeout(timer);
+        reject(new Error('URL is not a valid image.'));
+      };
+      img.onload = () => {
+        clearTimeout(timer);
+        cachedUrls[url] = true;
+        resolve();
+      };
+      timer = setTimeout(() => {
+        img.src = '';
+        reject(new Error('Timed out while validating image.'));
+      }, timeout);
+      img.src = url;
+    });
+  };
+
   // TODO: why does setting the same image twice fail?
   if (isUrl(input.value)) {
-    testImage(input.value)
+    const box = document.querySelector('a-box'); // TODO: remove me
+    validateImage(input.value)
       .then(() => {
         box.setAttribute('src', input.value);
         box.setAttribute('color', 'white');
